3f6b9b70f1b5f7985c21e4eaca3967411fd0dfa0
robel <robelabraha11@gmail.com>
2024-06-05 23:38:24 -0700

    updated index.js and generatemarkdown.js

---
+++ README.md
@@ -0,0 +1,43 @@
+# README-GENERATOR
+    ![License: APACHE 2.0](https://img.shields.io/badge/License-APACHE%202.0-blue.svg)
+  
+  ## Description
+  
+  generate readme file
+  
+  ## Table of Contents
+  
+  * [Installation](#installation)
+  * [Usage](#usage)
+  
+* [License](#license)
+
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation
+  
+  installation
+  
+  ## Usage
+  
+  usage
+  
+  ## Contributing
+  
+  contribute
+  
+  ## Tests
+  
+  tests
+  
+  ## Questions
+  
+  If you have any questions about the repo, open an issue or contact me directly at Bartorino10@gmail.com. You can find more of my work at [RobelA11](https://github.com/RobelA11/).
+  
+  ## License
+  
+  This project is licensed under the APACHE 2.0 license.
+  
+  
--- index.js
+++ index.js
@@ -1,85 +1,74 @@
-
+// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { error } = require('console');
-const generateMarkdown = require('./generatemarkDown');
-
-// add the question here
-const questions = [ 
-{
-    typpe: 'input',
-    name: 'title',
-    message: 'what is the title of your project',
-},
-{
-    type: 'input',
-    name: 'description',
-    message: 'Provide a description of your project:',   
-},
-{
-    type: 'input',
-    name: 'installation',
-    message: 'What are the installation instructions?',  
-},
-{
-    type: 'input',
-    name: 'usage',
-    message: 'What is the usage information?',  
-},
-{
-    type: 'input',
-    name: 'contributing',
-    message: 'What are the contribution guidelines?',
-},
-{
-    type: 'input',
-    name: 'tests',
-    message: 'What are the test instructions?', 
-},
-{
-    type: 'input',
-    name: 'license',
-    message: 'What is the license for your project?',
-},
-
-{
-    type: 'input',
-    name: 'github',
-    message: 'What is your GitHub username?', 
-},
-
-{
-     type: 'input',
-     name: 'email',
-     message: 'What is your email address?',
-}
-
+const generateMarkdown = require('/utils/generateMarkdown');
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project:',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide usage information:',
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'Provide contribution guidelines:',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide test instructions:',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+      },
 ];
 
-// function to write README file
+// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) =>
-     err ? console.log(err) : console.log('Successfully created README file!')
-
-
-    );
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+          return console.log(err);
+        }
+        console.log('Success! Your README.md file has been generated');
+      });
 }
 
-
-// function to initialize app
+// TODO: Create a function to initialize app
 function init() {
-    inquirer.createPromptModule(questions)
-        .then((answers) => {
-            const markdownContent = generateMarkdown(answers);
-            writeToFile('README.md', markdownContent);
-
-
-    })
-    .catch((error) => {
-        console.error(error);
-    });
+    inquirer.prompt(questions).then((responses) => {
+        const markdown = generateMarkdown(responses);
+        writeToFile('README.md', markdown);
+      });
 }
 
 // Function call to initialize app
 init();
-
